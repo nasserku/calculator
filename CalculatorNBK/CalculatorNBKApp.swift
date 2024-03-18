0e2c4dc75532a0601eaa79a1c2b3a0231e221be5
naser <zm1n@live.com>
2024-03-18 10:04:24 +0300

    Initial Commit

---
+++ CalculatorNBK/CalculatorNBKApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorNBKApp.swift
+//  CalculatorNBK
+//
+//  Created by Naser on 18/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorNBKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
