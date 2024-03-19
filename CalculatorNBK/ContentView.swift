7298a7c9fed598f405a948d548bdb0163c396de1
naser <zm1n@live.com>
2024-03-19 10:51:04 +0300

    first commit

--- CalculatorNBK/ContentView.swift
+++ CalculatorNBK/ContentView.swift
@@ -7,18 +7,67 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    var firstNumber = ""
+    var secondNumber = ""
+    var currentOperation = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func numberButtonPressed(_ sender: UIButton) {
+        guard let digit = sender.title(for: .normal) else { return }
+        displayLabel.text! += digit
+    }
+    
+    @IBAction func operationButtonPressed(_ sender: UIButton) {
+        guard let operation = sender.title(for: .normal) else { return }
+        if !displayLabel.text!.isEmpty {
+            firstNumber = displayLabel.text!
+            currentOperation = operation
+            displayLabel.text = ""
         }
-        .padding()
+    }
+    
+    @IBAction func equalsButtonPressed(_ sender: UIButton) {
+        if !displayLabel.text!.isEmpty {
+            secondNumber = displayLabel.text!
+            let num1 = Double(firstNumber)!
+            let num2 = Double(secondNumber)!
+            var result = 0.0
+            switch currentOperation {
+            case "+":
+                result = num1 + num2
+            case "-":
+                result = num1 - num2
+            case "*":
+                result = num1 * num2
+            case "/":
+                if num2 != 0 {
+                    result = num1 / num2
+                } else {
+                    displayLabel.text = "Error"
+                    return
+                }
+            default:
+                break
+            }
+            displayLabel.text = "\(result)"
+        }
+    }
+    
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        displayLabel.text = ""
+        firstNumber = ""
+        secondNumber = ""
+        currentOperation = ""
     }
 }
 
-#Preview {
-    ContentView()
-}
