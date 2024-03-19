fbe71f3e993dd062b47b8622b433b2c31225ee2f
naser <zm1n@live.com>
2024-03-19 10:57:51 +0300

    change error message

--- CalculatorNBK/ContentView.swift
+++ CalculatorNBK/ContentView.swift
@@ -53,7 +53,7 @@ class ViewController: UIViewController {
                 if num2 != 0 {
                     result = num1 / num2
                 } else {
-                    displayLabel.text = "Error"
+                    displayLabel.text = "Try again, error"
                     return
                 }
             default:
